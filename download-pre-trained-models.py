8494f16fa3859b3d83ba8fc828e7a4b5a75070f8
Cameron Smith <camerons03@vt.edu>
2025-05-04 12:32:18 -0400

    Added sample image for testing, downloader for pretrained model

---
+++ download-pre-trained-models.py
@@ -0,0 +1,53 @@
+# I learned the hard way that pushing multi gig datasets to GitHub is a bad idea
+# and extremely painful to fix in the history.
+
+# Running this will download it which is already in the gitignore, thus wont be
+# pushed to the repo.
+
+import io
+import os
+import zipfile
+
+import requests
+from tqdm import tqdm
+
+
+def download_with_progress(url, filename):
+    response = requests.get(url, stream=True)
+    total_size = int(response.headers.get("content-length", 0))
+    block_size = 1024
+    with open(filename, "wb") as file, tqdm(
+        desc=filename,
+        total=total_size,
+        unit="iB",
+        unit_scale=True,
+        unit_divisor=1024,
+    ) as bar:
+        for data in response.iter_content(block_size):
+            bar.update(len(data))
+            file.write(data)
+
+
+# I used my ftp server to host the dataset, just makes it easier
+dataset_link = "https://ftp.cameronwsmith.com/pub/other/pre-trained.zip"
+local_file = "./handwriting-dataset.zip"
+
+
+def main():
+    if os.path.exists("./pre-trained"):
+        print("Models already downloaded")
+        return
+
+    print("Downloading dataset... (this may take a while)")
+    download_with_progress(dataset_link, local_file)
+
+    print("Done downloading, extracting dataset...")
+    with zipfile.ZipFile(local_file, "r") as zip_ref:
+        zip_ref.extractall("./")
+
+    print("Dataset extracted")
+    os.remove(local_file)
+
+
+if __name__ == "__main__":
+    main()
